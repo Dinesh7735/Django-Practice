f5478929ab1bd8d3df56417d5036c68f34096aa0
dinesh7735 <naikdinesh544@gmail.com>
2026-04-08 13:13:39 +0530

    Understanding the concept of specific url mappping

---
+++ project11/aap/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def ashish(request):
+    return render(request,'ashish.html')
+def subha(request):
+    return render(request,'subha.html')
